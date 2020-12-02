9b8019846e0a7aa1262ad214020046ad7353e99d
Christoph Meyer <christoph@chmey.com>
2020-12-02 10:28:09 +0100

    Implement IP2ASN querying

--- iptoasn/server.py
+++ iptoasn/server.py
@@ -4,20 +4,17 @@ import requests
 import os
 import shutil
 import pandas
+import json
 import ipaddress
 
 
-class IPtoASN(HTTPServer, BaseHTTPRequestHandler):
-    def do_GET(self):
-        pass
-
+class IPtoASN(HTTPServer):
     def __init__(self, DB_DIR='./', DB_FNAME='ip2asn.tsv.gz'):
         self.dirDB = DB_DIR
         self.fnameDB = DB_FNAME
         self.updateDB()
         self.loadDB()
-        print(self.queryIP("1.1.1.1"))
-        super().__init__(('localhost', 8080), self)
+        super().__init__(('localhost', 8080), self.RequestHandler)
 
     def updateDB(self):
         URL = "https://iptoasn.com/data/ip2asn-combined.tsv.gz"
@@ -40,3 +37,38 @@ class IPtoASN(HTTPServer, BaseHTTPRequestHandler):
         uintIP = int(ipaddress.ip_address(IP))  # noqa: F841
         queryStr = "uint_range_start <= @uintIP <= uint_range_end"
         return self.db.query(queryStr)
+
+    class RequestHandler(BaseHTTPRequestHandler):
+        def sendJson(self, j, status=200):
+            self.send_response(status)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(j).encode('utf-8'))
+
+        def do_GET(self):
+            args = self.path.split('/')
+            if args[1] == 'api' and args[2] == 'ip':
+                row = self.server.queryIP(args[3]).to_dict(orient='records')[0]
+                if row:
+                    j = {
+                        'ip': args[3],
+                        'as_number': row['AS_number'],
+                        'as_country_code': row['country_code'],
+                        'as_description': row['AS_description'],
+                        'range_start': row['range_start'],
+                        'range_end': row['range_end']
+                    }
+                    self.sendJson(j, status=200)
+                else:
+                    j = {
+                        "status": "error",
+                        "message": "Could not resolve IP to ASN."
+                    }
+                    self.sendJson(j, status=400)
+            else:
+                j = {
+                    "status": "error",
+                    "message": "Could not understand the request.",
+                    "solution": "Use path /api/ip/<IP> to query the service."
+                }
+                self.sendJson(j, status=400)
