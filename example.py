807dcc2fcc65e5f78dbe8506bb0f5ab221bbab86
Christoph Meyer <christoph@chmey.com>
2020-12-02 13:06:05 +0100

    Always output startup indicator.

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
 from iptoasn import IPtoASN
 server = IPtoASN(host='', port=8080)  # Listen on *:8080
-print("ASN-Database loaded. Starting API..")
+print("ASN-Database loaded. Starting API..", flush=True)
 server.serve_forever()
